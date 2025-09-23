516d095206d3eda1e5e45ffd53aad314eb7baa9a
AvivKermann <avivkermann@gmail.com>
2025-09-23 09:04:24 +0300

    added more sketchy bar and aerospace

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -2,6 +2,7 @@
 
 
 export BAR_COLOR="0xff181926"
+export BG_COLOR="0x40ffffff"
 export ITEM_BG_COLOR="0xffe0def4"
 export ACCENT_COLOR="0xffe0def4"
 export TEXT_COLOR="0xffebbcba"
---
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# make sure it's executable with:
+
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    sketchybar --set $NAME background.drawing=on
+else
+    sketchybar --set $NAME background.drawing=off
+fi
